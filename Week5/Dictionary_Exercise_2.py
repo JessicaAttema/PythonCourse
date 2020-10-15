1367339e53ed1b942e0e55ba8f58fbd23ed7c1dc
JessicaAttema <jessicaattema@hotmail.com>
2020-10-15 14:34:25 +0200

    New assignments

---
+++ Week5/Dictionary_Exercise_2.py
@@ -0,0 +1,12 @@
+def add_fruit(inventory, fruit, quantity=0):
+    inventory[fruit] = quantity
+    return inventory
+
+
+# Make these tests work...
+new_inventory = {}
+add_fruit(new_inventory, "strawberries", 10)
+print("strawberries" in new_inventory)
+print(new_inventory["strawberries"] == 10)
+add_fruit(new_inventory, "strawberries", 25)
+print(new_inventory["strawberries"] == 35)
